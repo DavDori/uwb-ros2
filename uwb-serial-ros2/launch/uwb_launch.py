1a2766cbe1084a164c64ecad56e3feb2477fcb59
DavDori <davide.dorigoni@unitn.it>
2024-09-26 17:38:37 +0200

    add launch files

--- uwb-serial-ros2/launch/uwb_launch.py
+++ uwb-serial-ros2/launch/uwb_launch.py
@@ -3,25 +3,20 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import Node
-
+import launch_ros.actions
 
 def generate_launch_description():
     config = os.path.join(
-        get_package_share_directory('lidar_turret'),
+        get_package_share_directory('uwb_serial'),
         'config',
-        'lidar_turret.yaml',
+        'params.yaml',
         )
-    
+    print(config)
     return LaunchDescription([
-        Node(
-            package='lidar_turret',
-            executable='turret',
+        launch_ros.actions.Node(
+            package='uwb_serial',
+            executable='uwb',
             output='screen',
             parameters=[config],
-        ),
-        Node(
-            package='rplidar_ros',
-            executable='rplidar_a3_launch.py',
         ),
     ])
